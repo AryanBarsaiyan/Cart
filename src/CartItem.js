2658aba75bb2e2eeea1d82b409a4a7393af25220
Aryan Barsaiyan <82722871+AryanBarsaiyan@users.noreply.github.com>
2022-11-01 19:30:58 +0530

    add props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,64 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: "Mobile Phone",
-            qty: 1,
-            img: ''
-        }
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         price: 999,
+    //         title: "Mobile Phone",
+    //         qty: 1,
+    //         img: ''
+    //     }
+    //     // this.increaseQuantity = this.increaseQuantity.bind(this);
+    //     // this.testing();
+    // }
+    // testing() {
+    //     const promise = new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve('done');
+    //         }, 5000);
+    //     })
+    // promise.then(() => {
+    //     // setState acts like a synchronous call
+    //     this.setState({ qty: this.state.qty + 10 });
+    //     this.setState({ qty: this.state.qty + 10 });
+    //     this.setState({ qty: this.state.qty + 10 });
+    //     console.log('state', this.state);
+    // });
+    // }
+    
+    increaseQuantity = () => {  // arrow function
+        // console.log('this', this.state);
+        // {this.state.qty += 1}
+        
+        // setState form 1
+
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+
+        // setState form 2 - if prevState required use this
+
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
     }
+    decreaseQuantity = () => {
+        const { qty } = this.state;
+        if (qty === 0) {
+            return;
+        }
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+            }
+        });
+    }
+    
   render() {
-    const { price, title, qty } = this.state;
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -22,9 +69,16 @@ class CartItem extends React.Component {
           <div style={{ color: "#777" }}>Rs {price}</div>
           <div style={{ color: "#777" }}>Qty: {qty}</div>
           <div className="cart-item-actions">{/* Buttons */}</div>
-          <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
-          <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
-          <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+          <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
+            // onClick={this.increaseQuantity.bind(this)}
+             onClick={this.increaseQuantity}
+          />
+          <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+            onClick={this.decreaseQuantity}
+          />
+          <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/484/484662.png" 
+            onClick = {() => this.props.onDeleteItem(this.props.product.id)}
+          />
         </div>
       </div>
     );
